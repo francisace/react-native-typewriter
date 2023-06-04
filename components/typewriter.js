5a10e4303a6520bfde505b537aa3a8edc20bc780
frank <frank@franks-MBP.fios-router.home>
2023-06-04 15:53:34 -0400

    Added |

--- components/typewriter.js
+++ components/typewriter.js
@@ -164,7 +164,7 @@ export default class TypeWriter extends Component {
     const { visibleChars } = this.state;
     const component = (
       <Text {...rest}>
-        {children}
+        {children}|
       </Text>
     );
 
